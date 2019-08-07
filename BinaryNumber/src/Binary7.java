c464a652378ad6210d1324a14081b3324a1aa552
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2019-08-07 17:40:01 +0530

    Ajinkya - Diaplay a binary number in other number system

---
+++ BinaryNumber/src/Binary7.java
@@ -0,0 +1,15 @@
+/**
+ * Created by lenovo on 8/7/2019.
+ */
+public class Binary7 {
+
+    public static void main(String[] args) {
+        int num = 0b1010;
+
+        System.out.printf("\nIn Decimal = %d",num);
+        System.out.printf("\nIn Octal = %o",num);
+        System.out.printf("\nIn Hexadecimal = %x",num);
+        System.out.printf("\nIn Binary = %s",Integer.toBinaryString(num));
+    }
+
+}
