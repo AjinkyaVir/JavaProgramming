09b31a524972997d5f1460a1902f28062c7e04f0
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2019-08-07 17:22:25 +0530

    Ajinkya - sum of two numbers

---
+++ SumOfTwoNumbers/src/Sum.java
@@ -0,0 +1,14 @@
+/**
+ * Created by lenovo on 8/7/2019.
+ */
+public class Sum {
+
+    public static void main(String[] args) {
+        int numberOne = 10;
+        int numberTwo = 15;
+
+        System.out.println("Addition of two numbers : " + (numberOne + numberTwo));
+
+    }
+
+}
