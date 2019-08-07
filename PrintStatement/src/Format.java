7b96cb3d51d2050e8c0726221c262b2ac00ed0ef
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2019-08-07 17:32:08 +0530

    Ajinkya - Effect of print statement and println statement

---
+++ PrintStatement/src/Format.java
@@ -0,0 +1,16 @@
+/**
+ * Created by lenovo on 8/7/2019.
+ */
+public class Format {
+
+    public static void main(String[] args) {
+        int a = 2; int b = 3; int c = 4;
+        System.out.print(a + "\t" + b);
+        System.out.print(b + "\n" + b);
+        System.out.print(" : " + c);
+        System.out.println();
+        System.out.println("Hello\\Hi\""+c);
+
+    }
+
+}
