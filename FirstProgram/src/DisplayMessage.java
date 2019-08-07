ef450477eeae8b4e235c8ef828386cbb17e16da8
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2019-08-07 17:16:29 +0530

    Ajinkya - Display Message program

---
+++ FirstProgram/src/DisplayMessage.java
@@ -0,0 +1,8 @@
+/**
+ * Created by lenovo on 8/7/2019.
+ */
+public class DisplayMessage {
+    public static void main(String[] args) {
+        System.out.println("This is my first program");
+    }
+}
