6371c7466a9947841a655ac00b1bd98f8020671c
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2019-08-08 13:18:01 +0530

    Ajinkya- check color by using switch

---
+++ ExampleOfSwitchStatement/src/Test.java
@@ -0,0 +1,29 @@
+/**
+ * Created by lenovo on 8/8/2019.
+ */
+public class Test {
+    public static void main(String[] args) {
+        char color ='x';
+
+        switch(color){
+            case 'g' :
+                System.out.println("Color is green");
+                break;
+            case   'b':
+                System.out.println("Color is blue");
+                break;
+            case   'y':
+                System.out.println("Color is yello");
+                break;
+            case   'r':
+                System.out.println("Color is red");
+                break;
+            default:
+                System.out.println("No color");
+        }
+    }
+
+
+
+
+}
