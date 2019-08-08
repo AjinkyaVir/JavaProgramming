5c379191566250f9fec3235983ad72b4f0864142
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2019-08-08 13:08:21 +0530

    Ajinkya - Diaplay number by using Infinit Loop

---
+++ DisplayNumberOneToTenInfinitLoop/src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created by lenovo on 8/8/2019.
+ */
+public class Test {
+    public static void main(String[] args) {
+        int i = 1;
+        System.out.println("Display number One To Ten: ");
+        for ( ; ; ){
+            System.out.println(i);
+            i++;
+            if (i > 10) break;
+        }
+    }
+}
