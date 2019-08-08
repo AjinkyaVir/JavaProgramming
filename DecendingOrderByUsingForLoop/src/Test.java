2899248e01eabd811120054226352cd11b516b00
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2019-08-08 13:24:05 +0530

    Ajinkya- show number in descending order by using for loop

---
+++ DecendingOrderByUsingForLoop/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by lenovo on 8/8/2019.
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        for (int i =10 ; i>=1; i--){
+            System.out.println(i);
+        }
+
+    }
+}
