325a3fb7b637f3032371cb3faa240625ee39bbff
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2019-08-08 13:08:54 +0530

    Ajinkya - Star Program

---
+++ StarProgram/src/Star.java
@@ -0,0 +1,19 @@
+/**
+ * Created by lenovo on 8/8/2019.
+ */
+public class Star {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        for (int i = 1; i<=x; i++){
+
+            for (int j = 1; j<=i; j++){
+                System.out.println(" * ");
+
+            }
+            System.out.println();
+        }
+    }
+
+}
