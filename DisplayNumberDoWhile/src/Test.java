d25c2904c69f483756a04392787d009573fc0779
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2019-08-08 13:09:39 +0530

    Ajinkya - Example of Do..while loop

---
+++ DisplayNumberDoWhile/src/Test.java
@@ -0,0 +1,17 @@
+/**
+ * Created by lenovo on 8/8/2019.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        int number = 1;
+        System.out.println("Number 1 to 10: " );
+        do{
+            System.out.println(number);
+            number++;
+        }while(number <=10);
+
+    }
+
+}
