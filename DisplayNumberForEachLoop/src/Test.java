ce8fc737212b241a3d01b22caa3590f99c094443
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2019-08-08 13:07:37 +0530

    Ajinkya - Diaplay number by using for Each loop

---
+++ DisplayNumberForEachLoop/src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Created by lenovo on 8/8/2019.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        int arr[] = {11,12,-56,89,50};
+
+        for(int x : arr){
+            System.out.println(x);
+        }
+
+    }
+
+}
